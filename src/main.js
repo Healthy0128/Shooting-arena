cf6c676d0e1da4558340289291c1dc24c0c4a02e
Takumu Soma <128652629+Healthy0128@users.noreply.github.com>
2026-08-24 15:52:14 +0900

    Bump menu fit build entry

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { initMenuUI } from './ui.js?v=6104';
 
 initMenuUI();
-import('./game.js?v=6104').catch(err=>{
+import('./game.js?v=6105').catch(err=>{
   console.error('Failed to start game:',err);
 });
