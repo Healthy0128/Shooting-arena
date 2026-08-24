2bbb75baa3fe2b8c2d9b8e7e63dbc3d4b921a91f
Takumu Soma <128652629+Healthy0128@users.noreply.github.com>
2026-08-24 15:29:03 +0900

    Refresh menu UI build imports

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { initMenuUI } from './ui.js?v=696';
+import { initMenuUI } from './ui.js?v=6104';
 
 initMenuUI();
-import('./game.js?v=6103').catch(err=>{
+import('./game.js?v=6104').catch(err=>{
   console.error('Failed to start game:',err);
 });
